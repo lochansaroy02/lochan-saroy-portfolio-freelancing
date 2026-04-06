c2e8ed9d54cf44c2fe01d935a9d7a4779238cd8d
lochansaroy02 <lochansaroy47@gmail.com>
2026-04-06 23:20:18 +0530

    updated nextjs version

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete 
-    // even if your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [new URL('https://assets.example.com/account123/**')],
-  },
   /* config options here */
   async redirects() {
     return [
